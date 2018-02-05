6c372a61a8c84534bd0e738c5e7c4dd7b3bd936b
Edward Njoroge <codingedward@gmail.com>
2018-02-05 21:59:12 +0300

    added mapbox

--- assets/js/bootstrap.js
+++ assets/js/bootstrap.js
@@ -1,7 +1,9 @@
 window._ = require('lodash');
 window.axios = require('axios');
+window.Popper = require('popper.js/dist/umd/popper');
 try {
     window.$ = window.jQuery = require('jquery');
-    require('bootstrap-sass');
+    require('popper');
+    require('bootstrap');
 } catch (e) {}
 
