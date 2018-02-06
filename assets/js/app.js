be72abc5c0dc7d967171aec588732a3e1717bab6
Edward Njoroge <codingedward@gmail.com>
2018-02-06 18:27:40 +0300

    adding map grid

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,39 +1,80 @@
 require('./bootstrap');
-
 window.Vue = require('vue');
 window.mapboxgl = require('mapbox-gl');
 
+const API_VERSION   = 'api/v1';
+const API_HOST      = 'http://zunguka-api.herokuapp.com';
+const MAP_STYLE     = 'mapbox://styles/mapbox/streets-v9';
+const CENTER        = [37.0104, -1.0902];
+const MAP_CONTAINER = 'map';
+const ONLOAD_ZOOM   = 5;
+const ANIMATE_ZOOM  = 14;
+const ANIMATE_TIME  = 3500; // 3.5 seconds
+const ACCESS_TOKEN  = 'pk.eyJ1IjoiZ2l0YXVtb3NlczQiLCJhIjoiY2pjOWdhODg4MG9kYzJ3bzR0eHE0ZXVodyJ9.zU1cfiq9SWoVsTaPdoFnBQ';
+
 new Vue({
     el: '#app',
+    data: {
+        apiUrl: `${API_HOST}/${API_VERSION}`,
+        mapStyle: MAP_STYLE,
+        accessToken: ACCESS_TOKEN,
+        mapContainer: MAP_CONTAINER,
+        
+        center: CENTER,
+        onLoadZoom: ONLOAD_ZOOM,
+        animateZoom: ANIMATE_ZOOM,
+        animateDuration: ANIMATE_TIME,
+
+        map: {},
+    },
+    mounted: function () {
+        const vm = this;
+        mapboxgl.accessToken = vm.accessToken;
+        var map = new mapboxgl.Map({
+            container: vm.mapContainer,
+            style: vm.mapCenter,
+            center: vm.center,
+            zoom: vm.onLoadZoom,
+        });
+    }
 });
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZ2l0YXVtb3NlczQiLCJhIjoiY2pjOWdhODg4MG9kYzJ3bzR0eHE0ZXVodyJ9.zU1cfiq9SWoVsTaPdoFnBQ';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [37.0154, -1.0969],
-    zoom: 5,
-});
 
 $(function() { 
     map.resize(); 
-    var point = [37.0104, -1.0902];//[36.9404674, -1.0891056];
-    map.on('load', (e) => {
-        console.log('here');
-        if (map.areTilesLoaded()) {
 
-            map.off('load');
+    var point = [37.0104, -1.0902];
+    map.on('load', (e) => {
+        map.off('load');
+        setTimeout(function () {
 
             map.easeTo({
                 animate: true,
                 duration: 3500,
                 center: point,
-                zoom: 14.5,
-                ease: t =>  t * t // easeInQuad
+                zoom: 14,
+                ease: t =>  t * t * t // easeInCube
             });
-        }
+
+            map.addLayer({
+                id: 'boundary',
+                type: 'line',
+                source: {
+                    type: 'geojson',
+                    data: 'http://zunguka-api.herokuapp.com/api/v1/boundary/?format=json',
+                },
+                paint: {
+                    'line-color': 'orange',
+                }
+            });
+        }, 1000);
     });
 });
 
 
 
+/*
+var marker = new mapboxgl.Marker() 
+    .setLngLat(point) 
+    .addTo(map);
+    */
--- public/js/app.js
+++ public/js/app.js
@@ -11342,43 +11342,84 @@ module.exports = __webpack_require__(41);
 /***/ (function(module, exports, __webpack_require__) {
 
 __webpack_require__(12);
-
 window.Vue = __webpack_require__(37);
 window.mapboxgl = __webpack_require__(40);
 
-new Vue({
-    el: '#app'
-});
+var API_VERSION = 'api/v1';
+var API_HOST = 'http://zunguka-api.herokuapp.com';
+var MAP_STYLE = 'mapbox://styles/mapbox/streets-v9';
+var CENTER = [37.0104, -1.0902];
+var MAP_CONTAINER = 'map';
+var ONLOAD_ZOOM = 5;
+var ANIMATE_ZOOM = 14;
+var ANIMATE_TIME = 3500; // 3.5 seconds
+var ACCESS_TOKEN = 'pk.eyJ1IjoiZ2l0YXVtb3NlczQiLCJhIjoiY2pjOWdhODg4MG9kYzJ3bzR0eHE0ZXVodyJ9.zU1cfiq9SWoVsTaPdoFnBQ';
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiZ2l0YXVtb3NlczQiLCJhIjoiY2pjOWdhODg4MG9kYzJ3bzR0eHE0ZXVodyJ9.zU1cfiq9SWoVsTaPdoFnBQ';
-var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v9',
-    center: [37.0154, -1.0969],
-    zoom: 5
+new Vue({
+    el: '#app',
+    data: {
+        apiUrl: API_HOST + '/' + API_VERSION,
+        mapStyle: MAP_STYLE,
+        accessToken: ACCESS_TOKEN,
+        mapContainer: MAP_CONTAINER,
+
+        center: CENTER,
+        onLoadZoom: ONLOAD_ZOOM,
+        animateZoom: ANIMATE_ZOOM,
+        animateDuration: ANIMATE_TIME,
+
+        map: {}
+    },
+    mounted: function mounted() {
+        var vm = this;
+        mapboxgl.accessToken = vm.accessToken;
+        var map = new mapboxgl.Map({
+            container: vm.mapContainer,
+            style: vm.mapCenter,
+            center: vm.center,
+            zoom: vm.onLoadZoom
+        });
+    }
 });
 
 $(function () {
     map.resize();
-    var point = [37.0104, -1.0902]; //[36.9404674, -1.0891056];
-    map.on('load', function (e) {
-        console.log('here');
-        if (map.areTilesLoaded()) {
 
-            map.off('load');
+    var point = [37.0104, -1.0902];
+    map.on('load', function (e) {
+        map.off('load');
+        setTimeout(function () {
 
             map.easeTo({
                 animate: true,
                 duration: 3500,
                 center: point,
-                zoom: 14.5,
+                zoom: 14,
                 ease: function ease(t) {
-                    return t * t;
-                } // easeInQuad
+                    return t * t * t;
+                } // easeInCube
             });
-        }
+
+            map.addLayer({
+                id: 'boundary',
+                type: 'line',
+                source: {
+                    type: 'geojson',
+                    data: 'http://zunguka-api.herokuapp.com/api/v1/boundary/?format=json'
+                },
+                paint: {
+                    'line-color': 'orange'
+                }
+            });
+        }, 1000);
     });
 });
+
+/*
+var marker = new mapboxgl.Marker() 
+    .setLngLat(point) 
+    .addTo(map);
+    */
 
 /***/ }),
 /* 12 */
